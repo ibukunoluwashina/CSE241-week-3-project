06698af918b686be6f6d4b219ad16758c48e2f84
ibukunoluwashina <ibukunoluwashina@gmail.com>
2024-02-03 20:57:13 +0100

    update

--- data/database.js
+++ data/database.js
@@ -1,10 +1,28 @@
-// data/database.js
 const mongoose = require("mongoose");
 require("dotenv").config();
-const dbName = '';
 
-mongoose
-  .connect(process.env.DATABASE_URL)
-  .then(() => console.log("connect to the database")).catch((err) => {
-    console.log(err.message);
+const dbName = 'Task';
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log(`Connected to the ${dbName} database`))
+  .catch((error) => {
+    console.error(`Error connecting to the ${dbName} database: ${error.message}`);
+    process.exit(1); // Exit the process if the connection fails
   });
+
+mongoose.connection.on('disconnected', () => {
+  console.log(`Disconnected from the ${dbName} database. Reconnecting...`);
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(`Error during reconnection to the ${dbName} database: ${error.message}`);
+});
+
+module.exports = mongoose.connection;
